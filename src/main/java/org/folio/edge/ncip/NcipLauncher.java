4c71ac40804bbe151972c6420ae36ceced987bc7
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2024-05-17 19:36:56 +0500

    fixed manifest error

--- src/main/java/org/folio/edge/ncip/NcipLauncher.java
+++ src/main/java/org/folio/edge/ncip/NcipLauncher.java
@@ -1,13 +1,11 @@
 package org.folio.edge.ncip;
 
 import io.vertx.core.Launcher;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class NcipLauncher extends Launcher {
-  private static final Logger logger = LogManager.getLogger(NcipLauncher.class);
 
   public static void main(String[] args) {
+
     new NcipLauncher().dispatch(args);
   }
 }
