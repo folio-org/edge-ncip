b361fab7e44caf3e97377649fd481198b558d903
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2024-05-17 19:34:11 +0500

    fixed manifest error

--- src/main/java/org/folio/edge/ncip/NcipLauncher.java
+++ src/main/java/org/folio/edge/ncip/NcipLauncher.java
@@ -3,16 +3,11 @@ package org.folio.edge.ncip;
 import io.vertx.core.Launcher;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
-
-import java.security.Security;
 
 public class NcipLauncher extends Launcher {
   private static final Logger logger = LogManager.getLogger(NcipLauncher.class);
 
   public static void main(String[] args) {
-    Security.addProvider(new BouncyCastleFipsProvider());
-    logger.info("BouncyCastleFipsProvider has been added");
     new NcipLauncher().dispatch(args);
   }
 }
