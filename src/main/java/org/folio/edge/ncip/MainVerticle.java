244cc24cbc0543c1863dbaaf5424eda80ad64133
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2024-05-20 15:30:17 +0500

    fixed shade plugin issue

--- src/main/java/org/folio/edge/ncip/MainVerticle.java
+++ src/main/java/org/folio/edge/ncip/MainVerticle.java
@@ -3,18 +3,14 @@ package org.folio.edge.ncip;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.folio.edge.core.EdgeVerticleHttp;
 import org.folio.edge.core.utils.OkapiClientFactory;
 import org.folio.edge.ncip.utils.NcipOkapiClientFactory;
 
 public class MainVerticle extends EdgeVerticleHttp {
-  private static final Logger logger = LogManager.getLogger(MainVerticle.class);
 
   @Override
   public Router defineRoutes() {
-    logger.info("BouncyCastleFipsProvider has been added");
     OkapiClientFactory ocf = NcipOkapiClientFactory.createInstance(vertx, config());
     NcipHandler ncipHandler = new NcipHandler(secureStore, ocf);
     Router router = Router.router(vertx);
