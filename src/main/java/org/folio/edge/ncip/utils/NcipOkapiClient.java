5ac8e66ff16dea5d457f1a265b881d56c6345870
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2024-05-20 17:20:28 +0500

    code refactoring

--- src/main/java/org/folio/edge/ncip/utils/NcipOkapiClient.java
+++ src/main/java/org/folio/edge/ncip/utils/NcipOkapiClient.java
@@ -7,54 +7,52 @@ import io.vertx.ext.web.client.HttpResponse;
 import org.folio.edge.core.utils.OkapiClient;
 
 
-
-
 public class NcipOkapiClient extends OkapiClient {
 
-	  public NcipOkapiClient(OkapiClient client) {
-	    super(client);
-	  }
+  public NcipOkapiClient(OkapiClient client) {
+    super(client);
+  }
 
-	  public void ncipConfigCheck(String payload, MultiMap headers,
-			  Handler<HttpResponse<Buffer>> responseHandler, 
-			  Handler<Throwable> exceptionHandler) {
-		  
-		      System.out.println("in send get request in the client");
-			    get(
-			        String.format("%s/ncipconfigcheck", okapiURL),
-			        tenant,
-			        defaultHeaders,
-			        responseHandler,
-			        exceptionHandler);
-			  }
-	  
-	  
-	  public void ncipHealthCheck(String payload, MultiMap headers,
-			  Handler<HttpResponse<Buffer>> responseHandler, 
-			  Handler<Throwable> exceptionHandler) {
-		      System.out.println("in send get request in the client");
-			    get(
-			        String.format("%s/nciphealthcheck", okapiURL),
-			        tenant,
-			        defaultHeaders,
-			        responseHandler,
-			        exceptionHandler);
-	}
-	  
-	  
-	  public void callNcip(String payload, MultiMap headers,
-			  Handler<HttpResponse<Buffer>> responseHandler, 
-			  Handler<Throwable> exceptionHandler) {
-		      System.out.println(String.format("%s/ncip", okapiURL));
-			    post(
-			        String.format("%s/ncip", okapiURL),
-			        tenant,
-			        payload,
-			        defaultHeaders,
-			        responseHandler,
-			        exceptionHandler);
-			  }
-		    
+  public void ncipConfigCheck(String payload, MultiMap headers,
+                              Handler<HttpResponse<Buffer>> responseHandler,
+                              Handler<Throwable> exceptionHandler) {
+
+    System.out.println("in send get request in the client");
+    get(
+        String.format("%s/ncipconfigcheck", okapiURL),
+        tenant,
+        defaultHeaders,
+        responseHandler,
+        exceptionHandler);
+  }
+
+
+  public void ncipHealthCheck(String payload, MultiMap headers,
+                              Handler<HttpResponse<Buffer>> responseHandler,
+                              Handler<Throwable> exceptionHandler) {
+    System.out.println("in send get request in the client");
+    get(
+        String.format("%s/nciphealthcheck", okapiURL),
+        tenant,
+        defaultHeaders,
+        responseHandler,
+        exceptionHandler);
+  }
+
+
+  public void callNcip(String payload, MultiMap headers,
+                       Handler<HttpResponse<Buffer>> responseHandler,
+                       Handler<Throwable> exceptionHandler) {
+    System.out.println(String.format("%s/ncip", okapiURL));
+    post(
+        String.format("%s/ncip", okapiURL),
+        tenant,
+        payload,
+        defaultHeaders,
+        responseHandler,
+        exceptionHandler);
+  }
+
 }
 		  
 
