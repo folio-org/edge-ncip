036739efc9683b726a911a1d6b134da4ef07df1d
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2024-05-21 14:46:56 +0500

    fixed formatting

--- src/main/java/org/folio/edge/ncip/MainVerticle.java
+++ src/main/java/org/folio/edge/ncip/MainVerticle.java
@@ -12,29 +12,29 @@ import static org.folio.edge.core.Constants.SYS_REQUEST_TIMEOUT_MS;
 
 public class MainVerticle extends EdgeVerticleHttp {
 
-  private int reqTimeoutMs;
+	  private int reqTimeoutMs;
 
-  public MainVerticle() {
-    super();
-    if (System.getProperty(SYS_REQUEST_TIMEOUT_MS) != null) {
-      reqTimeoutMs = Integer.parseInt(System.getProperty(SYS_REQUEST_TIMEOUT_MS));
-    } else {
-      reqTimeoutMs = 35000;
-    }
-  }
+	  public MainVerticle() {
+	    super();
+	    if (System.getProperty(SYS_REQUEST_TIMEOUT_MS) != null) {
+	        reqTimeoutMs = Integer.parseInt(System.getProperty(SYS_REQUEST_TIMEOUT_MS));
+	      } else {
+	        reqTimeoutMs = 35000;
+	      }
+	  }
 
-  @Override
-  public Router defineRoutes() {
-    OkapiClientFactory ocf = OkapiClientFactoryInitializer.createInstance(vertx, config());
-    NcipHandler ncipHandler = new NcipHandler(secureStore, ocf);
-    Router router = Router.router(vertx);
-    router.route().handler(BodyHandler.create());
-    router.route(HttpMethod.POST, "/ncip/:apiKeyPath").handler(ncipHandler::handle);
-    router.route(HttpMethod.POST, "/ncip").handler(ncipHandler::handle);
-    router.route(HttpMethod.GET, "/ncipconfigcheck").handler(ncipHandler::handleConfigCheck);
-    router.route(HttpMethod.GET, "/nciphealthcheck").handler(ncipHandler::handleHealthCheck);
-    router.route(HttpMethod.GET, "/admin/health").handler(this::handleHealthCheck);
-    return router;
-  }
+	  @Override
+	  public Router defineRoutes() {
+      OkapiClientFactory ocf = OkapiClientFactoryInitializer.createInstance(vertx, config());
+	    NcipHandler ncipHandler = new NcipHandler(secureStore, ocf);
+	    Router router = Router.router(vertx);
+	    router.route().handler(BodyHandler.create());
+	    router.route(HttpMethod.POST, "/ncip/:apiKeyPath").handler(ncipHandler::handle);
+	    router.route(HttpMethod.POST, "/ncip").handler(ncipHandler::handle);
+	    router.route(HttpMethod.GET,"/ncipconfigcheck").handler(ncipHandler::handleConfigCheck);
+	    router.route(HttpMethod.GET, "/nciphealthcheck").handler(ncipHandler::handleHealthCheck);
+            router.route(HttpMethod.GET, "/admin/health").handler(this::handleHealthCheck);
+	    return router;
+	  }
 
 }
